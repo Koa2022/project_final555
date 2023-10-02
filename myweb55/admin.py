f3a6151b3fd189c8c886c203334ee34ac8c45d8f
Koa2022 <anuchakanya2.560@gmail.com>
2023-10-02 17:08:47 +0700

    upload

---
+++ myweb55/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from . import models
+
+
+# Register your models here.
+
+@admin.register(models.Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = [
+        'department'
+    ]
+
+@admin.register(models.Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = [
+        'fname',
+        'lname',
+        'gender',
+        'age',
+        'education',
+        'department',
+    ]
+
---
+++ myweb55/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Myweb55Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myweb55'
